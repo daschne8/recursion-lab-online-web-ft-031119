5d609ca07745517aff88e8a78a1ba53f459e4814
Daniel Schneider <daschne8@gmail.com>
2019-08-24 15:14:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function printString(string){
   if (string.length > 1) {
     console.log(string[0])
-    printString(string[1])
+    printString(string.substring(1,string.length))
   } else {
     console.log(string[0])
   }
