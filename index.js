8c0e3e121f812cd1e2d70b0286e420664aa2f4d0
Daniel Schneider <daschne8@gmail.com>
2019-08-24 15:26:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function printString(string){
 
 function reverseString(string){
   if (string.length > 1) {
-     return reverseString(string.substring(0,string.length-1)) + string[string.length-1]
+     return string[string.length-1] + reverseString(string.substring(0,string.length-1))
   } else {
     return string[0]
   }
