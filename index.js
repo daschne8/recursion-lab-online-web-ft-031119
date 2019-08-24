67b16f006f5a0e6f4538a940ae3950d4ef91b53b
Daniel Schneider <daschne8@gmail.com>
2019-08-24 15:03:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function printString(string){
   if (string.length > 1) {
+    console.log(string[0])
     return string[0] + printString(string[1])
   } else {
+    console.log(string[0])
     return string[0]
   }
 }
