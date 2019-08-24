3f0e70b3810bfd9ef0743421f1b56f3035d1bab1
Daniel Schneider <daschne8@gmail.com>
2019-08-24 15:25:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-function printString(string){
-  if (string.length > 1) {
-    console.log(string[0])
-    printString(string.substring(1,string.length))
-  } else {
-    console.log(string[0])
-  }
-}
-
 function reverseString(string){
   if (string.length > 1) {
-     return reverseString(string.substring(0,string.length-1))
+     return reverseString(string.substring(0,string.length-1)) + string[string.length-1]
   } else {
     return string[0]
   }
